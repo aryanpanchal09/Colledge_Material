be46e820a8af2be8048cc0028691f4e591ef021b
Aryan <Aryantech03@gmail.com>
2025-02-18 00:16:19 +0530

    -

--- Practical Subject/ApaniNews Project/ApaniNewz/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Registration
-
-# Register your models here.
-@admin.register(Registration)
-class UserModelAdmin(admin.ModelAdmin):
-    ist_display = ('first_name','last_name','username', 'email','phone', 'profile_image','password','confirm_password')
